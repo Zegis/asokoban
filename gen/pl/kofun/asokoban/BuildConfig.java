75d27d475d3398ec1ec3cfe393fdd69d89c483eb
Zegis <kjgm@poczta.onet.pl>
2016-12-27 17:14:44 +0100

    Removed unecessary files

--- gen/pl/kofun/asokoban/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package pl.kofun.asokoban;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/pl/kofun/asokoban/FlingDirection.java
+++
@@ -1,5 +0,0 @@
-package pl.kofun.asokoban;
-
-public enum FlingDirection {
-UP, DOWN, RIGHT, LEFT, NoN
-}
